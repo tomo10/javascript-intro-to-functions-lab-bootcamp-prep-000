029c2fd135a8dbb3f97b23df8e40b0c18c79d1e4
Thomas Edwards <thce28@gmail.com>
2019-01-30 17:34:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ function sayHiToGrandma(string) {
     return "YES INDEED!"
   }
   
-  else (string === "I love you, Grandma")
+  else if (string === "I love you, Grandma") {
    return "I love you, too."
+  }
 }
